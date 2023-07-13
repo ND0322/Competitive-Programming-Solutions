903e6abb7cbeaceadb08de0a32d314949534c1a0
ND0322 <98077043+ND0322@users.noreply.github.com>
2023-07-13 15:36:01 -0700

    Create J1.py

---
+++ CCC/2019/J1.py
@@ -0,0 +1,9 @@
+e = int(input()) * 3 + int(input()) * 2 + int(input())
+s = int(input()) * 3 + int(input()) * 2 + int(input())
+
+if e > s:
+  print("A")
+elif e < s:
+  print("B")
+else:
+  print("T")
